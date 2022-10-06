77e7ce9735b4b9fc69d583bb00f3d40c8382cedb
chucks <iamdanielchukwuma@gmail.com>
2022-10-06 18:14:55 +0100

    Add Note and Users form

---
+++ src/features/users/NewUserForm.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import { useAddNewUserMutation } from "./usersApiSlice";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave } from "@fortawesome/free-solid-svg-icons";
+import { ROLES } from "../../config/roles";
+
+const NewUserForm = () => {
+	return <div></div>;
+};
+
+export default NewUserForm;
--- src/features/users/usersApiSlice.js
+++ src/features/users/usersApiSlice.js
@@ -29,10 +29,50 @@ export const usersApiSlice = apiSlice.injectEndpoints({
 				} else return [{ type: "User", id: "LIST" }];
 			},
 		}),
+
+		addNewUser: builder.mutation({
+			query: (initialUserData) => ({
+				url: "/users",
+				method: "POST",
+				body: {
+					...initialUserData,
+				},
+			}),
+			invalidatesTags: [{ type: "User", id: "LIST" }],
+		}),
+
+		updateUser: builder.mutation({
+			query: (initialUserData) => ({
+				url: "/users",
+				method: "PATCH",
+				body: {
+					...initialUserData,
+				},
+			}),
+			invalidatesTags: (result, error, arg) => [
+				{ type: "User", id: arg.id },
+			],
+		}),
+		deleteUser: builder.mutation({
+			query: ({ id }) => ({
+				url: `/users/${id}`,
+				method: "DELETE",
+				body: { id },
+			}),
+			invalidatesTags: (result, error, arg) => [
+				{ type: "User", id: arg.id },
+			],
+		}),
 	}),
 });
 
-export const { useGetUsersQuery } = usersApiSlice;
+// RTK Query automatically creates hook for the query
+export const {
+	useGetUsersQuery,
+	useAddNewUserMutation,
+	useUpdateUserMutation,
+	useDeleteUserMutation,
+} = usersApiSlice;
 
 export const selectUsersResult = usersApiSlice.endpoints.getUsers.select();
 
